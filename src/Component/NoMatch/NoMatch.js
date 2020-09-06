0ef2d3144e7a90b2a435eb872aa53cd752e63054
iamshakibb <shihabunshakib9346@gmail.com>
2020-09-07 03:48:06 +0600

    Upload all Folder

--- src/Component/NoMatch/NoMatch.js
+++ src/Component/NoMatch/NoMatch.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const notmathcStyle = makeStyles({
+  NotMatchHeader: {
+    marginTop: "25%",
+    color: "red",
+  },
+});
 
 const NoMatch = () => {
+  const classes = notmathcStyle();
   return (
     <div>
-      <Typography variant="h6" gutterBottom>
-        Not Found
+      <Typography variant="h1" gutterBottom align="center" className={classes.NotMatchHeader}>
+        Sorry , Not Found
       </Typography>
     </div>
   );
